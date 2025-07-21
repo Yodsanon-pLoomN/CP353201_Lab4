ec35a4eab256d709ec119bc1d73a4c296889fc53
Yodsanon-pLoomN <pLoomN321654987@gmail.com>
2025-07-21 13:49:48 +0700

    แก้ไขตาราง

--- Lab4_BVT/MembershipRanking/src/sqa/test/RankingTestNorm.java
+++ Lab4_BVT/MembershipRanking/src/sqa/test/RankingTestNorm.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -21,19 +20,19 @@ class RankingTestNorm {
 	@DisplayName("Normal boundary value testing")
 	@ParameterizedTest
 	@CsvSource({
-		"0,4,500,Standard",
-		"1,4,500,Standard",
-		"50000,4,500,Gold",
-		"99999,4,500,Gold",
-		"100000,4,500,Gold",
+		"0,15,500,Standard",
+		"1,15,500,Standard",
+		"50000,15,500,Standard",
+		"99999,15,500,Standard",
+		"100000,15,500,Standard",
 		"50000,0,500,Standard",
-		"50000,1,500,Silver",
-		"50000,6,500,Standard",
-		"50000,7,500,Standard",
-		"50000,4,0,Standard",
-		"50000,4,1,Standard",
-		"50000,4,999,Gold",
-		"50000,4,1000,Gold"
+		"50000,1,500,Standard",
+		"50000,29,500,Standard",
+		"50000,30,500,Standard",
+		"50000,15,0,Standard",
+		"50000,15,1,Standard",
+		"50000,15,999,Standard",
+		"50000,15,1000,Standard"
 	})
 	void testBoundary(int purchaseTotal, int frequency, int pointCollected, String expected) {
 		assertEquals(expected, testRanking.CalculateMembershipRank(purchaseTotal, frequency, pointCollected));
--- Lab4_BVT/MembershipRanking/src/sqa/test/RankingTestRobust.java
+++ Lab4_BVT/MembershipRanking/src/sqa/test/RankingTestRobust.java
@@ -20,25 +20,25 @@ class RankingTestRobust {
 	@DisplayName("Robustness testing")
 	@ParameterizedTest
 	@CsvSource({
-		"-1,4,500,Invalid Input",
-		"0,4,500,Standard",
-		"1,4,500,Standard",
-		"50000,4,500,Gold",
-		"99999,4,500,Gold",
-		"100000,4,500,Gold",
-		"100001,4,500,Standard",
+		"-1,15,500,Invalid Input",
+		"0,15,500,Standard",
+		"1,15,500,Standard",
+		"50000,15,500,Standard",
+		"99999,15,500,Standard",
+		"100000,15,500,Standard",
+		"100001,15,500,Standard",
 		"50000,-1,500,Invalid Input",
 		"50000,0,500,Standard",
-		"50000,1,500,Silver",
-		"50000,6,500,Standard",
-		"50000,7,500,Standard",
-		"50000,8,500,Standard",
-		"50000,4,-1,Invalid Input",
-		"50000,4,0,Standard",
-		"50000,4,1,Standard",
-		"50000,4,999,Gold",
-		"50000,4,1000,Gold",
-		"50000,4,1001,Standard"
+		"50000,1,500,Standard",
+		"50000,29,500,Standard",
+		"50000,30,500,Standard",
+		"50000,31,500,Standard",
+		"50000,15,-1,Invalid Input",
+		"50000,15,0,Standard",
+		"50000,15,1,Standard",
+		"50000,15,999,Standard",
+		"50000,15,1000,Standard",
+		"50000,15,1001,Standard"
 	})
 	void testBoundary(int purchaseTotal, int frequency, int pointCollected, String expected) {
 		assertEquals(expected, testRanking.CalculateMembershipRank(purchaseTotal, frequency, pointCollected));
